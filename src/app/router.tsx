800878225305f067325b8e6c85f43adc53b417e7
Lee-geon-gyu <gungyu0203@gmail.com>
2026-07-21 05:03:27 +0900

    Configure Deployment

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -7,43 +7,48 @@ import { ResponsiveWebPage } from '../pages/ResponsiveWebPage'
 import { WebLibrariesPage } from '../pages/WebLibrariesPage'
 import { App } from './App'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        lazy: async () => {
-          const { HomePage } = await import('../pages/HomePage')
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          lazy: async () => {
+            const { HomePage } = await import('../pages/HomePage')
 
-          return { Component: HomePage }
+            return { Component: HomePage }
+          },
         },
-      },
-      {
-        path: 'reference',
-        element: <ReferencePage />,
-      },
-      {
-        path: 'ai',
-        element: <AiToolsPage />,
-      },
-      {
-        path: 'resources',
-        element: <DesignResourcesPage />,
-      },
-      {
-        path: 'responsive-web',
-        element: <ResponsiveWebPage />,
-      },
-      {
-        path: 'libraries',
-        element: <WebLibrariesPage />,
-      },
-      {
-        path: 'publishing-guide',
-        element: <PublishingGuidePage />,
-      },
-    ],
+        {
+          path: 'reference',
+          element: <ReferencePage />,
+        },
+        {
+          path: 'ai',
+          element: <AiToolsPage />,
+        },
+        {
+          path: 'resources',
+          element: <DesignResourcesPage />,
+        },
+        {
+          path: 'responsive-web',
+          element: <ResponsiveWebPage />,
+        },
+        {
+          path: 'libraries',
+          element: <WebLibrariesPage />,
+        },
+        {
+          path: 'publishing-guide',
+          element: <PublishingGuidePage />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: import.meta.env.BASE_URL,
   },
-])
+)
